d1f6b9ba400f43fafa67463dcc334efc334f6538
mxc-commons <frank.hein@maxence.de>
2013-11-15 05:34:07 +0100

    added a space to fix confused github client

--- src/MxcGenerics/Stdlib/Hydrator/ClassMethods.php
+++ src/MxcGenerics/Stdlib/Hydrator/ClassMethods.php
@@ -4,7 +4,7 @@
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
  * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @license   http://framework.zend.com/license/new-bsd New BSD License 
  */
 
 namespace MxcGenerics\Stdlib\Hydrator;
