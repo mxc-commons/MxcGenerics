20be815168dd9e6d1e2d29446e4dbdde39bcfee8
mxc-commons <frank.hein@maxence.de>
2014-02-04 17:11:49 +0100

    loadOptions works recursively now.
    Settings from chosen optionset ($name) now get merged recursively to the
    defaults.

--- src/MxcGenerics/Stdlib/GenericOptions.php
+++ src/MxcGenerics/Stdlib/GenericOptions.php
@@ -3,6 +3,7 @@
 namespace MxcGenerics\Stdlib;
 
 use MxcGenerics\Exception;
+use Zend\Stdlib\ArrayUtils;
 
 /** 
  * Generic Options expects
@@ -31,18 +32,27 @@ class GenericOptions extends GenericRegistry {
     public function __construct(array $options, $name = 'defaults') {
         $this->loadOptions($options, $name);
     }
-    
+
+    /**
+     * Recursively merge optionset $name to default settings
+     * 
+     * @param array $options
+     * @param string $name
+     * @throws Exception\InvalidArgumentException
+     */
     protected function loadOptions($options, $name) {
         // load default options first
-        $this->setProperties(isset($options['defaults']) ? $options['defaults'] : array());
-        if ($name === 'defaults') return;
-        //--- silently ignore non strings
-        if (is_string($name)) {
-            if (!isset($options['options'][$name]))
-                throw new Exception\InvalidArgumentException(
-                	sprintf('Options not found: %s',  $name));
-            // override/extend with option set    
-            $this->setProperties($options['options'][$name]);
-        }
+        $opt = isset($options['defaults']) ? $options['defaults'] : array();
+		if (is_string($name)) {
+            if ($name != 'defaults') {
+                if (!isset($options['options'][$name])) {
+                    throw new Exception\InvalidArgumentException(
+                    	sprintf('Options not found: %s',  $name));
+                }
+                $optionSet = $options['options'][$name];
+                $opt = ArrayUtils::merge($opt, $optionSet);
+            }
+		}
+        $this->setProperties($opt);
     }
 }
